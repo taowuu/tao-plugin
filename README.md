ca48b37ff76175ad908c96772312d8a7745b59a3
Taowuu <taowuu@foxmail.com>
2022-11-06 09:39:31 +0800

    第一步完成

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # tao-plugin
- 一步步实现 Webpack plugin
+
+## 什么是 plugin
+
+## 第一步
+- `npm install`
+- 配置 webpack config
+- 新建 plugin 目录文件
+- 新建 待转化的 md 文件
+- 新建 接受内容的 html 文件
+
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const { resolve } = require('path')
+const MdToHtmlPlugin = require('./plugins/md-to-html-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: resolve(__dirname, 'src/app.js'),
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: 'app.js',
+  },
+  devtool:'source-map',
+  plugins: [
+    new MdToHtmlPlugin({
+      // 要解析的文件
+      template: resolve(__dirname, 'test.md'),
+      // 放入的文件
+      filename: 'test.html'
+    })
+  ],
+}
